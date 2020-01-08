ee6b693788c6aa1779a2001fd5a9ae67a1144f3e
Ben <ben@thecloudmethod.com>
2020-01-08 13:00:07 -0800

    added process.env.port

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
+const PORT = process.env.PORT || 4001;
+
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.enableCors({
     methods: 'GET',
     maxAge: 3600,
   });
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap().catch(err => console.error(err));
